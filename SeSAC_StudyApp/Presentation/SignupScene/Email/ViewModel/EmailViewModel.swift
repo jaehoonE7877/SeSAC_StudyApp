82461c8f5c56467288ad07f980e28d153435164b
jaehoon <sjh7877@naver.com>
2022-11-10 22:23:31 +0900

    Feat: Email ViewModel 구현, emailValdation logic 생성

--- SeSAC_StudyApp/Presentation/SignupScene/Email/ViewModel/EmailViewModel.swift
+++ SeSAC_StudyApp/Presentation/SignupScene/Email/ViewModel/EmailViewModel.swift
@@ -10,8 +10,38 @@ import Foundation
 import RxCocoa
 import RxSwift
 
-final class EmailViewModel {
+final class EmailViewModel: ViewModelType {
     
+    struct Input {
+        let emailText: ControlProperty<String>
+        let nextButtonTapped: ControlEvent<Void>
+        let textFieldBeginEdit: ControlEvent<Void>
+        let textFieldEndEdit: ControlEvent<Void>
+    }
     
+    struct Output {
+        let emailValid: Driver<Bool>
+        let nextButtonTapped: ControlEvent<Void>
+        let textFieldBeginEdit: ControlEvent<Void>
+        let textFieldEndEdit: ControlEvent<Void>
+    }
     
+    func transform(input: Input) -> Output {
+        
+        let emailValid = input.emailText
+            .distinctUntilChanged()
+            .withUnretained(self)
+            .map { weakSelf, text in
+                weakSelf.validateEmail(text)
+            }
+            .asDriver(onErrorJustReturn: false)
+        
+        return Output(emailValid: emailValid, nextButtonTapped: input.nextButtonTapped, textFieldBeginEdit: input.textFieldBeginEdit, textFieldEndEdit: input.textFieldEndEdit)
+    }
+    
+    private func validateEmail(_ text: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: text)
+    }
 }
