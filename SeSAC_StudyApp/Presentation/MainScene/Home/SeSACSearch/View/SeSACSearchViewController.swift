aabdcdac1dc5bd1ee585700d8dfda6d5ab33755f
jaehoon <sjh7877@naver.com>
2022-11-23 04:38:32 +0900

    Feat: TabMan 라이브러리 추가

---
+++ SeSAC_StudyApp/Presentation/MainScene/Home/SeSACSearch/View/SeSACSearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SeSACSearchViewController.swift
+//  SeSAC_StudyApp
+//
+//  Created by Seo Jae Hoon on 2022/11/23.
+//
+
+import UIKit
+
+final class SeSACSearchViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
